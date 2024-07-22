971d9301f6ab31cc6c92be75b2afb7270bf11b1d
Ciraldos <andreaciraldo2@gmail.com>
2024-07-22 16:37:22 +0200

    .

---
+++ Giorno_1/Esercitazione_M5_Seconda_Settimana/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Esercitazione_M5_Seconda_Settimana.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Esercitazione_M5_Seconda_Settimana.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Giorno_1/Esercitazione_M5_Seconda_Settimana/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Esercitazione_M5_Seconda_Settimana.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
