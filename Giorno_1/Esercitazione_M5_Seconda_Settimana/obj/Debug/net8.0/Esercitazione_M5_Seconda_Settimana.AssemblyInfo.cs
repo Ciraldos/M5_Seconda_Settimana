cdab784c16a521a22454bd854f4d0bef091f689a
Ciraldos <andreaciraldo2@gmail.com>
2024-07-26 15:38:00 +0200

    .

--- Giorno_1/Esercitazione_M5_Seconda_Settimana/obj/Debug/net8.0/Esercitazione_M5_Seconda_Settimana.AssemblyInfo.cs
+++ Giorno_1/Esercitazione_M5_Seconda_Settimana/obj/Debug/net8.0/Esercitazione_M5_Seconda_Settimana.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Esercitazione_M5_Seconda_Settimana")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6eccc6c09218cd5bba23440d932def1bfb652b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4515cd283075a06281ad185b8faa4afea88d192")]
 [assembly: System.Reflection.AssemblyProductAttribute("Esercitazione_M5_Seconda_Settimana")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Esercitazione_M5_Seconda_Settimana")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Giorno_1/Esercitazione_M5_Seconda_Settimana/wwwroot/js/site.js
+++ Giorno_1/Esercitazione_M5_Seconda_Settimana/wwwroot/js/site.js
@@ -19,19 +19,24 @@ function getPrenotazioniByCf() {
             let container = $("#prenotazioniContainer");
             container.empty();
             $(data).each((_, prenotazione) => {
-                let card = $(`<div class="card mt-3">
-            <div class="card-body">
-            <h5 class="card-title">Prenotazione ID: ${prenotazione.idPrenotazione}</h5>
-            <p class="card-text">NumProgressivo: ${prenotazione.numProgressivo}</p>
-            <p class="card-text">Anno: ${prenotazione.anno}</p>
-            <p class="card-text">SoggiornoDal: ${prenotazione.soggiornoDal}</p>
-            <p class="card-text">SoggiornoAl: ${prenotazione.soggiornoAl}</p>
-            <p class="card-text">Caparra: ${prenotazione.caparra}</p>
-            <p class="card-text">Tariffa: ${prenotazione.tariffa}</p>
-            <p class="card-text">PensioneCompleta: ${prenotazione.pensioneCompleta}</p>
-            <p class="card-text">IdCliente: ${prenotazione.idCliente}</p>
-            <p class="card-text">IdCamera: ${prenotazione.idCamera}</p>
-            </div>
+                let card = $(`<div class="col-md-4 mt-3">
+                <div class="card card-custom">
+                    <div class="card-header-custom">
+                        Dettagli Prenotazione
+                    </div>
+                    <div class="card-body card-body-custom">
+                        <p class="card-text"><i class="fas fa-id-badge icon"></i><span>ID Prenotazione:</span> ${prenotazione.idPrenotazione}</p>
+                        <p class="card-text"><i class="fas fa-list-ol icon"></i><span>Num Progressivo:</span> ${prenotazione.numProgressivo}</p>
+                        <p class="card-text"><i class="fas fa-calendar-alt icon"></i><span>Anno:</span> ${prenotazione.anno}</p>
+                        <p class="card-text"><i class="fas fa-calendar-day icon"></i><span>Soggiorno Dal:</span> ${prenotazione.soggiornoDal}</p>
+                        <p class="card-text"><i class="fas fa-calendar-check icon"></i><span>Soggiorno Al:</span> ${prenotazione.soggiornoAl}</p>
+                        <p class="card-text"><i class="fas fa-money-check-alt icon"></i><span>Caparra:</span> €${prenotazione.caparra}</p>
+                        <p class="card-text"><i class="fas fa-dollar-sign icon"></i><span>Tariffa:</span> €${prenotazione.tariffa}</p>
+                        <p class="card-text"><i class="fas fa-bed icon"></i><span>Pensione Completa:</span> ${prenotazione.pensioneCompleta}</p>
+                        <p class="card-text"><i class="fas fa-user icon"></i><span>Id Cliente:</span> ${prenotazione.idCliente}</p>
+                        <p class="card-text"><i class="fas fa-door-open icon"></i><span>Id Camera:</span> ${prenotazione.idCamera}</p>
+                    </div>
+                </div>
             </div>`);
                 container.append(card);
             });
@@ -53,19 +58,24 @@ function getPrenotazioniByPensione() {
             let container = $("#prenotazioniContainer");
             container.empty();
             $(data).each((_, prenotazione) => {
-                let card = $(`<div class="card mt-3">
-            <div class="card-body">
-            <h5 class="card-title">Prenotazione ID: ${prenotazione.idPrenotazione}</h5>
-            <p class="card-text">NumProgressivo: ${prenotazione.numProgressivo}</p>
-            <p class="card-text">Anno: ${prenotazione.anno}</p>
-            <p class="card-text">SoggiornoDal: ${prenotazione.soggiornoDal}</p>
-            <p class="card-text">SoggiornoAl: ${prenotazione.soggiornoAl}</p>
-            <p class="card-text">Caparra: ${prenotazione.caparra}</p>
-            <p class="card-text">Tariffa: ${prenotazione.tariffa}</p>
-            <p class="card-text">PensioneCompleta: ${prenotazione.pensioneCompleta}</p>
-            <p class="card-text">IdCliente: ${prenotazione.idCliente}</p>
-            <p class="card-text">IdCamera: ${prenotazione.idCamera}</p>
-            </div>
+                let card = $(`<div class="col-md-4 mt-3">
+                <div class="card card-custom">
+                    <div class="card-header-custom">
+                        Dettagli Prenotazione
+                    </div>
+                    <div class="card-body card-body-custom">
+                        <p class="card-text"><i class="fas fa-id-badge icon"></i><span>ID Prenotazione:</span> ${prenotazione.idPrenotazione}</p>
+                        <p class="card-text"><i class="fas fa-list-ol icon"></i><span>Num Progressivo:</span> ${prenotazione.numProgressivo}</p>
+                        <p class="card-text"><i class="fas fa-calendar-alt icon"></i><span>Anno:</span> ${prenotazione.anno}</p>
+                        <p class="card-text"><i class="fas fa-calendar-day icon"></i><span>Soggiorno Dal:</span> ${prenotazione.soggiornoDal}</p>
+                        <p class="card-text"><i class="fas fa-calendar-check icon"></i><span>Soggiorno Al:</span> ${prenotazione.soggiornoAl}</p>
+                        <p class="card-text"><i class="fas fa-money-check-alt icon"></i><span>Caparra:</span> €${prenotazione.caparra}</p>
+                        <p class="card-text"><i class="fas fa-dollar-sign icon"></i><span>Tariffa:</span> €${prenotazione.tariffa}</p>
+                        <p class="card-text"><i class="fas fa-bed icon"></i><span>Pensione Completa:</span> ${prenotazione.pensioneCompleta}</p>
+                        <p class="card-text"><i class="fas fa-user icon"></i><span>Id Cliente:</span> ${prenotazione.idCliente}</p>
+                        <p class="card-text"><i class="fas fa-door-open icon"></i><span>Id Camera:</span> ${prenotazione.idCamera}</p>
+                    </div>
+                </div>
             </div>`);
                 container.append(card);
             });
